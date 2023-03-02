c190b9e4d99860ef507488fbd76de9127bbb8234
Anastasiya Kormilitsina <anastasiya.kormilitsina@gmail.com>
2023-03-03 00:13:12 +0500

    i need more refactotring

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,96 @@
 import Searchbar from 'components/Searchbar';
 import ImageGallery from 'components/ImageGallery';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import css from '../App/App.module.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
+import Loader from 'components/Loader';
+import { getPictures } from 'services/getPictures';
+import style from '../Button/Button.module.css';
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [items, setItems] = useState([]);
   const [request, setRequest] = useState('');
+  const [showButton, setShowButton] = useState(false);
+  const [loader, setShowLoader] = useState(false);
+  const [total, setTotal] = useState(0);
+  const [error, setError] = useState(null);
 
-  const formSubmitHandler = request => {
-    setRequest(request);
+  const formSubmitHandler = query => {
+    setQuery(query);
   };
+
+  const buttonСlickHandler = () => {
+    setPage(page + 1);
+  };
+  useEffect(() => {
+    setRequest(query);
+    setPage(1);
+    setShowButton(false);
+    setItems([]);
+  }, [query]);
+
+  useEffect(() => {
+    async function fetch(request, page) {
+      try {
+        setShowLoader(true);
+        const { hits: pictures, totalHits: total } = await getPictures(
+          request,
+          page
+        );
+        setTotal(total);
+
+        if (pictures.length) {
+          setShowButton(true);
+          setItems([...items, ...pictures]);
+          return;
+        }
+        toast.error('There are no images for your request');
+      } catch (error) {
+        setError(error);
+      } finally {
+        setShowLoader(false);
+      }
+    }
+    if (request) {
+      fetch(request, page);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page, request]);
+
+  useEffect(() => {
+    if (page * 12 >= total) {
+      setShowButton(false);
+    }
+  }, [page, total]);
 
   return (
     <div className={css.App}>
-      <Searchbar onSubmit={request => formSubmitHandler(request)} />
-      <ImageGallery value={request} />
+      <Searchbar onSubmit={query => formSubmitHandler(query)} />
+      {loader && <Loader />}
+      {error ? (
+        toast.error('Ooops, something went wrong')
+      ) : (
+        <>
+          <ImageGallery items={items} />
+          {showButton && (
+            <button
+              className={style.Button}
+              type="button"
+              onClick={buttonСlickHandler}
+            >
+              Load more
+            </button>
+          )}
+        </>
+      )}
+
       <ToastContainer />
     </div>
   );
 }
+
 export default App;
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,100 +1,28 @@
-import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { toast } from 'react-toastify';
 import ImageGalleryItem from 'components/ImageGalleryItem';
-import Loader from 'components/Loader';
 import css from '../ImageGallery/ImageGallery.module.css';
 import styles from '../ImageGalleryItem/ImageGalleryItem.module.css';
-import { getPictures } from 'services/getPictures';
-import style from '../Button/Button.module.css';
 
-function ImageGallery({ value }) {
-  const [items, setItems] = useState([]);
-  const [page, setPage] = useState(1);
-  const [showButton, setShowButton] = useState(false);
-  const [request, setRequest] = useState('');
-  const [showLoader, setShowLoader] = useState(false);
-  const [total, setTotal] = useState(0);
-  const [error, setError] = useState(null);
-
-  const buttonСlickHandler = () => {
-    setPage(page + 1);
-  };
-
-  useEffect(() => {
-    setRequest(value);
-    setPage(1);
-    setShowButton(false);
-    setItems([]);
-  }, [value]);
-
-  useEffect(() => {
-    if (request !== '') {
-      setShowLoader(true);
-      try {
-        fetch(request, page);
-      } catch {
-        setError(error);
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page, request]);
-
-  useEffect(() => {
-    if (page * 12 >= total) {
-      setShowButton(false);
-    }
-  }, [page, total]);
-
-  async function fetch(request, page) {
-    const { hits: pictures, totalHits: total } = await getPictures(
-      request,
-      page
-    );
-    setTotal(total);
-    setShowLoader(false);
-    if (pictures.length) {
-      setShowButton(true);
-      setItems([...items, ...pictures]);
-    } else {
-      toast.error('There are no images for your request');
-      setPage(1);
-      setShowButton(false);
-      setItems([]);
-    }
-  }
-
+function ImageGallery({ items }) {
   return (
-    <>
-      {showLoader && <Loader />}
-      <ul className={css.ImageGallery}>
-        {items.map(({ id, webformatURL, largeImageURL, tags }) => {
-          return (
-            <li key={id} className={styles.ImageGalleryItem}>
-              <ImageGalleryItem
-                max={largeImageURL}
-                min={webformatURL}
-                alt={tags.split(',')[0]}
-              />
-            </li>
-          );
-        })}
-      </ul>
-      {showButton && (
-        <button
-          className={style.Button}
-          type="button"
-          onClick={buttonСlickHandler}
-        >
-          Load more
-        </button>
-      )}
-    </>
+    <ul className={css.ImageGallery}>
+      {items.map(({ id, webformatURL, largeImageURL, tags }) => {
+        return (
+          <li key={id} className={styles.ImageGalleryItem}>
+            <ImageGalleryItem
+              max={largeImageURL}
+              min={webformatURL}
+              alt={tags.split(',')[0]}
+            />
+          </li>
+        );
+      })}
+    </ul>
   );
 }
 
 ImageGallery.propTypes = {
-  value: PropTypes.string.isRequired,
+  items: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
 };
 
 export default ImageGallery;
